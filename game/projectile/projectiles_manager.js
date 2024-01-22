2ae0be0b9d672423c42b148aededd20fdbb69f92
otaviomalvess <otaviomalvess@gmail.com>
2024-01-22 18:01:51 -0300

    refactor: - Created dedicated script for the projectiles manager.
    feat:
    - Function to instantiate projectiles.
    - Add and remove projectile from the stage.
    - Remove all projectiles from stage.
    - Docs.

---
+++ game/projectile/projectiles_manager.js
@@ -0,0 +1,97 @@
+import { AnimatedSprite, Assets } from '../../node_modules/pixi.js/dist/pixi.min.mjs';
+import { app } from '../index.js'; // @WORKAROUND: not a fan of doing this.
+import * as Projectile from './projectile.js';
+import * as Player from '../player/player.js';
+
+
+const projectiles = [];
+
+let spritesheet;
+
+
+/**
+ * @returns `Assets.load` promise.
+ */
+const load = async () => {
+    const promise = Assets.load('game/projectile/projectiles_sheet.json');
+    await promise.then((projectilesSheet) => {
+        spritesheet = projectilesSheet;
+    });
+
+    return promise;
+};
+
+
+/**
+ * @param {Float} dt 
+ */
+const physicsTick = (dt) => {
+    projectiles.forEach((p) => {
+        Projectile.move(p, dt);
+    });
+};
+
+
+/**
+ * Adds the projectile to the array and to the stage.
+ * @param {Object} projectile 
+ */
+const addProjectile = (projectile) => {
+    projectiles.push(projectile);
+    app.stage.addChild(projectile.animSprite);
+};
+
+
+/**
+ * @returns {Array} the player projectile animation from the spritesheet.
+ */
+const getPlayerProjectileSprite = () => {
+    return spritesheet.animations.player_proj;
+};
+
+
+/**
+ * Instantiates a projectile at the given initial position.
+ * The projectile animation is chosen randomly.
+ * @param {Object} initialPosition 
+ */
+const instantiateProjectile = (initialPosition) => {
+    const anim = Math.floor(Math.random() * 3 + 1);
+    const p = Projectile.create();
+    p.animSprite = new AnimatedSprite(spritesheet.animations[`proj_${anim}`]);
+    p.verticalDirection = 1;
+    p.onHit = Player.onHit;
+    Projectile.ready(p, initialPosition);
+    
+    addProjectile(p);
+};
+
+
+/**
+ * Removes the given projectile from the array of projectiles.
+ * @param {Object} projectile.
+ */
+const removeProjectile = (projectile) => {
+    const id = projectiles.findIndex((value) => projectile === value);
+    projectiles.splice(id, 1);
+};
+
+
+/**
+ * Removes all projectiles from stage and clears the array of projectiles.
+ */
+const reset = () => {
+    projectiles.forEach((p) => {
+        app.stage.removeChild(p.animSprite)
+        
+        // @TODO: this shouldn't be here.
+        if (p.fromPlayer) {
+            p.shot = false;
+        }
+    });
+    projectiles.length = 0;
+    
+};
+
+
+export { addProjectile, getPlayerProjectileSprite, instantiateProjectile, load, physicsTick, projectiles, removeProjectile, reset };
